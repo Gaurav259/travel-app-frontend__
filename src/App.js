f641408e17aa1df6c64626b733e169a5e70157a1
Gaurav259 <ragethakurgs18@gmail.com>
2025-04-27 11:02:44 +0530

    feat: added home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
-import './App.css';
+import { Home } from './pages';
+
+import "./App.css";
 
 function App() {
   return (
-    <h1>hello</h1>
-  );
+<Home/>  
+ );
 }
 
 export default App;
---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,74 @@
+import { useEffect, useState } from "react";
+import { useCategory } from "../../context";
+import axios from "axios";
+import "./Categories.css";
+
+export const Categories = () => {
+  const [categories, setCategories] = useState([]);
+  const [numberOfCategoryToShow, setNumberOfCategoryToShow] = useState(0);
+  const {hotelCategory,setHotelCategory} = useCategory();
+
+  const handleRightButtonClick = () => {
+    setNumberOfCategoryToShow((prev) => prev + 10);
+  };
+  const handleLeftButtonClick = () => {
+    setNumberOfCategoryToShow((prev) => prev - 10);
+  };
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await axios.get(
+          "https://travel-app-backend-rbzs.onrender.com/api/category"
+        );
+        const categoriesToShow = data.slice(
+          numberOfCategoryToShow + 10 > data.length
+            ? data.length - 10
+            : numberOfCategoryToShow,
+          numberOfCategoryToShow > data.length
+            ? data.length
+            : numberOfCategoryToShow + 10
+        );
+        setCategories(categoriesToShow);
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+  }, [numberOfCategoryToShow]);
+
+  const handleCategoryClick = (category) => {
+    console.log({category})
+     setHotelCategory(category);   
+  }
+  console.log({hotelCategory})
+
+
+
+  return (
+    <section className="categories d-flex align-center gap-large cursor-pointer shadow">
+      {numberOfCategoryToShow >= 10 && (
+        <button
+          className="button btn-category btn-left fixed cursor-pointer"
+          onClick={handleLeftButtonClick}
+        >
+          <span className="material-icons-outlined">chevron_left</span>
+        </button>
+      )}
+
+      {categories &&
+        categories.map(({ _id, category }) => (
+          <span  className={`${category  === hotelCategory ? "border-bottom" : ""}`} key={_id} onClick={() => handleCategoryClick(category)}>
+            {category}
+          </span>
+        ))}
+      {numberOfCategoryToShow - 10 < categories.length && (
+        <button
+          className="button btn-category btn-right fixed cursor-pointer"
+          onClick={handleRightButtonClick}
+        >
+          <span className="material-icons-outlined">chevron_right</span>
+        </button>
+      )}
+    </section>
+  );
+};
